243d4bb785de0717316f46032f8f074ea77741fe
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2021-11-16 17:06:57 +0300

    v 0.0.1

--- ShutdownTimer/Form1.Designer.cs
+++ ShutdownTimer/Form1.Designer.cs
@@ -29,20 +29,81 @@ namespace ShutdownTimer
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.counterTxt = new System.Windows.Forms.MaskedTextBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.counterLbl = new System.Windows.Forms.Label();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // counterTxt
+            // 
+            this.counterTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.counterTxt.Location = new System.Drawing.Point(218, 43);
+            this.counterTxt.Mask = "00:00:00";
+            this.counterTxt.Name = "counterTxt";
+            this.counterTxt.Size = new System.Drawing.Size(100, 34);
+            this.counterTxt.TabIndex = 0;
+            this.counterTxt.ValidatingType = typeof(int);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(228, 93);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Başlat";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // counterLbl
+            // 
+            this.counterLbl.AutoSize = true;
+            this.counterLbl.Location = new System.Drawing.Point(234, 164);
+            this.counterLbl.Name = "counterLbl";
+            this.counterLbl.Size = new System.Drawing.Size(64, 17);
+            this.counterLbl.TabIndex = 5;
+            this.counterLbl.Text = "00:00:00";
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.Location = new System.Drawing.Point(228, 122);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 6;
+            this.cancelBtn.Text = "İptal";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            this.cancelBtn.Click += new System.EventHandler(this.cancelBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(582, 336);
+            this.ClientSize = new System.Drawing.Size(536, 209);
+            this.Controls.Add(this.cancelBtn);
+            this.Controls.Add(this.counterLbl);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.counterTxt);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sayaç";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MaskedTextBox counterTxt;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label counterLbl;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- ShutdownTimer/Form1.cs
+++ ShutdownTimer/Form1.cs
@@ -7,14 +7,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace ShutdownTimer
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(counterTxt.Text))
+            {
+                string[] texts = counterTxt.Text.Split(':');
+                int hours = string.IsNullOrWhiteSpace(texts[0]) ? 0 : Convert.ToInt32(texts[0]);
+                int mins = string.IsNullOrWhiteSpace(texts[1]) ? 0 : Convert.ToInt32(texts[1]);
+                int secs = string.IsNullOrWhiteSpace(texts[2]) ? 0 : Convert.ToInt32(texts[2]);
+                counterLbl.Text = hours.ToString() + ':' + mins.ToString() + ':' + secs.ToString();
+                int totalTimeInSec = secs + (60 * mins) + (60 * 60 * hours);
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/C shutdown /s /t " + totalTimeInSec.ToString();
+                process.StartInfo = startInfo;
+                process.Start();
+                timer1.Start();
+            }
+        }
+
+        private void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C shutdown /a";
+            process.StartInfo = startInfo;
+            process.Start();
+            timer1.Stop();
+            counterLbl.Text = "00:00:00";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] texts = counterLbl.Text.Split(':');
+            int hours = Convert.ToInt32(texts[0]);
+            int mins = Convert.ToInt32(texts[1]);
+            int secs = Convert.ToInt32(texts[2]);
+            if (counterLbl.Text != "00:00:00")
+            {
+                secs--;
+                if (secs == 0 && mins != 0)
+                {
+                    mins--;
+                    secs = 59;
+                }
+                if (mins == 0 && hours != 0)
+                {
+                    hours--;
+                    mins = 59;
+                }
+                counterLbl.Text = fill(hours) + ':' + fill(mins) + ':' + fill(secs);
+            }
+        }
+        private string fill(int i)
+        {
+            if (i < 10)
+            {
+                return "0" + i.ToString();
+            }
+            else return i.ToString();
         }
     }
 }
